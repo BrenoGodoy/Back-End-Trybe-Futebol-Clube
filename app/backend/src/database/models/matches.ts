0311d50bb235cded8bff09197a87bb0a49adcb76
Breno Godoy <godoyy25z@gmail.com>
2022-10-03 19:01:27 -0300

    cria migration e model para tabela matches

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,47 @@
+import { Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: number;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: INTEGER,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Matches;
