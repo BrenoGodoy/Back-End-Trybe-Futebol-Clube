10c40f40e2f80610c18c25567941e243738ae478
Breno Godoy <godoyy25z@gmail.com>
2022-10-10 14:48:24 -0300

    fix: message do middleware

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -6,7 +6,9 @@ const loginValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
   if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
-  if (!regex.test(email)) return res.status(400).json({ message: 'All fields must be filled' });
+  if (!regex.test(email)) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
 
   next();
 };
