1ac46bafba23ef5c47475b71e378d88e436879b6
Breno Godoy <godoyy25z@gmail.com>
2022-10-20 03:07:46 -0300

    41%

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -2,49 +2,20 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+// import Token from '../helpers/Token'
+
 import { app } from '../app';
-import Matches from '../database/models/matches';
-import mock from './macthesMock';
-import Teams from '../database/models/teams';
+
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-interface IMatch {
-  homeTeam: number;
-  homeTeamGoals: number;
-  awayTeam: number;
-  awayTeamGoals: number;
-  inProgress: boolean;
-  teamHome: Teams;
-  teamAway: Teams;
-}
-
-// describe('/matches', () => {
-//   describe('GET-ONE', () => {
-//     before(() => {
-//       sinon.stub(Matches, 'findOne').resolves(mock as IMatch[]);
-//     });
-//     after(() => {
-//       sinon.restore();
-//     });
-//     it('', async () => {
-//       const response = (await chai.request(app).get('/teams/5'));
-//       chai.expect(response.status).to.equal(200);
-//       chai.expect(response.body).to.deep.equal({id: 5, teamName: 'Cruzeiro'});
-//     });
-//   });
-//   describe('GET-ALL', () => {
-//     before(() => {
-//       sinon.stub(Matches, 'findAll').resolves(mock as Matches[]);
-//     });
-//     after(() => {
-//       sinon.restore();
-//     });
-//     it('', async () => {
-//       const response = (await chai.request(app).get('/teams'));
-//       chai.expect(response.status).to.equal(200);
-//       chai.expect(response.body).to.deep.equal(mock);
-//     });
-//   })
-// })
+describe('/matches', () => {
+  describe('GET-ALL', () => {
+    it('', async () => {
+      const response = await chai.request(app).get('/matches');
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.be.an('array');
+    });
+  });
+})
---
+++ app/backend/src/tests/validate.test.ts
@@ -0,0 +1,28 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import generateToken from '../helpers/token'
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('/login/validate', () => {
+  describe('GET-ROLE', () => {
+    it('', async () => {
+      const data = {
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+      }
+      const token = generateToken(data.password, data.role);
+      const response = await chai.request(app).get('/login/validate').set('Authorization', token);
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.deep.equal({ role: 'user' });
+    });
+  });
+})
